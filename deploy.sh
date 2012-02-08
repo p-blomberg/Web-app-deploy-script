1ba6ef517ac5f886920bcac2f853e43e558a0282
David Sveningsson <ext@sidvind.com>
2012-02-08 02:01:44 +0100

    Major overhaul, support for colors, shows detailed error messages, hooks.

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,85 @@
 # nullglob is required for globbing release-list (if no releases exists)
 shopt -s nullglob
 
+# check if stdout is a terminal...
+if [ -t 1 ]; then
+		# see if it supports colors...
+		ncolors=$(tput colors)
+
+		if test -n "$ncolors" && test $ncolors -ge 8; then
+				bold="$(tput bold)"
+				underline="$(tput smul)"
+				standout="$(tput smso)"
+				normal="$(tput sgr0)"
+				black="$(tput setaf 0)"
+				red="$(tput setaf 1)"
+				green="$(tput setaf 2)"
+				yellow="$(tput setaf 3)"
+				blue="$(tput setaf 4)"
+				magenta="$(tput setaf 5)"
+				cyan="$(tput setaf 6)"
+				white="$(tput setaf 7)"
+		fi
+fi
+
+errstar="${bold}${red} * ${normal}"
+infostar="${bold}${green} * ${normal}"
+
 ################ Functions ###########################
+
+die(){
+		echo
+		echo "${errstar} ERROR: during stage ${stage}:"
+		echo "${errstar}   $*"
+		if ! rm $LOCK_FILE; then
+				echo "${errstar}"
+				echo "${errstar} Failed to delete lock file."
+		fi
+		local frame=0
+
+		set -o pipefail
+		echo "${errstar}"
+		echo "${errstar} Callstack:"
+		while caller $frame | awk -v errstar="${errstar}" '{print errstar "   "$3":"$1" in "$2}'; do
+				((frame++));
+		done
+
+		echo "${errstar}"
+		echo "${errstar} Variables:"
+		echo "${errstar}   VCS: ${VCS}"
+		echo "${errstar}   RELEASE_WC: ${RELEASE_WC}"
+		echo "${errstar}   EXPORT_TARGET: ${EXPORT_TARGET}"
+		echo "${errstar}   SYMLINK_PATH: ${SYMLINK_PATH}"
+		echo "${errstar}   DST: ${DST}"
+
+		exit 1
+}
+
+phase_start(){
+		local desc=""
+		if [[ $# -gt 0 ]]; then
+				desc=": $*"
+		fi
+		echo "${infostar} Running \"${stage}\"${desc}"
+}
+
+phase_end(){
+		echo "${infostar} .. OK"
+}
 
 update_pre_hook(){
 		return 0
 }
 
 update_repo_cmd() {
-	case $VCS in
-		svn)
-			echo "svn up \"$RELEASE_WC\""
-			;;
-		git)
-			echo "(cd \"$RELEASE_WC\"; git pull)"
-			;;
-	esac
+		case $VCS in
+				svn)
+						echo "svn up \"$RELEASE_WC\""
+						;;
+				git)
+						echo "(cd \"$RELEASE_WC\"; git pull)"
+						;;
+		esac
 }
 
 update_post_hook(){
@@ -26,22 +90,11 @@ update_post_hook(){
 
 do_update(){
 		stage="update"
-		echo "***** Stage: update"
-		if ! update_pre_hook; then
-				echo "update_pre_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 3
-		fi
-		eval `update_repo_cmd`
-		if [ $? -eq 0 ]; then
-				echo "***** Update completed"
-		else
-				echo "***** Unable to do update local repository" >&2
-				delete_lock_file_and_exit 3
-		fi
-		if ! update_post_hook; then
-				echo "update_post_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 3
-		fi
+		phase_start
+		update_pre_hook || die "update_pre_hook returned non-zero status, aborting"
+		eval `update_repo_cmd` || die "Unable to do update local repository"
+		update_post_hook || die "update_post_hook returned non-zero status, aborting"
+		phase_end
 }
 
 export_pre_hook(){
@@ -49,14 +102,14 @@ export_pre_hook(){
 }
 
 export_repo_cmd() {
-	case $VCS in
-		svn)
-			echo "svn export \"$RELEASE_WC\" \"$EXPORT_TARGET/$releasename\""
-			;;
-		git)
-			echo "(cd \"$RELEASE_WC\"; git checkout-index -a -f --prefix=\"$EXPORT_TARGET/$releasename/\" )"
-			;;
-	esac
+		case $VCS in
+				svn)
+						echo "svn export \"$RELEASE_WC\" \"${DST}\""
+						;;
+				git)
+						echo "(cd \"$RELEASE_WC\"; git checkout-index -a -f --prefix=\"${DST}/\" )"
+						;;
+		esac
 }
 
 export_post_hook(){
@@ -66,48 +119,22 @@ export_post_hook(){
 
 do_export(){
 		stage="export"
-		echo "***** Stage: export"
-		DST="${EXPORT_TARGET}/${releasename}"
-		if ! export_pre_hook; then
-				echo "export_pre_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 4
-		fi
-		eval `export_repo_cmd`
-		if [ $? -eq 0 ]; then
-        echo "***** Export created"
-		else
-        echo "***** Unable to create export" >&2
-        delete_lock_file_and_exit 4
-		fi
-		if ! export_post_hook; then
-				echo "export_post_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 4
-		fi
+		phase_start
+		export_pre_hook ||  die "export_pre_hook returned non-zero status, aborting"
+		eval `export_repo_cmd` || die "Unable to create export"
+		export_post_hook || die "export_post_hook returned non-zero status, aborting"
+		phase_end
 }
 
 file_prune(){
 		if [[ ${stage} != "export" ]]; then
-				echo "file_prune called from stage \"${stage}\", not \"export\" as it should"
-				delete_lock_file_and_exit 4
+				die "file_prune called from stage \"${stage}\", not \"export\" as it should"
 		fi
 		if [[ ${PRUNE:+1} ]]; then
 				for pattern in "${PRUNE[@]}"; do
 						find "${EXPORT_TARGET}/$releasename" -regex "${pattern}" -delete -printf "%p removed\n"
 				done
 		fi
-}
-
-delete_lock_file_and_exit() {
-	# Delete lock file
-	rm $LOCK_FILE
-	if [ $? -eq 0 ]; then
-		echo "***** Lock file deleted successfully."
-	else
-		echo "***** Failed to delete lock file." >&2
-		exit 666
-	fi
-
-	exit $1	
 }
 
 symlink_pre_hook(){
@@ -120,18 +147,11 @@ function symlink_update() {
 		
 		# remove old symlink
 		if [[ -L $dst ]]; then
-				
-				if ! rm $dst; then
-						echo "***** Unable to delete production symlink" >&2
-						delete_lock_file_and_exit 5
-				fi
+				rm $dst || die "Unable to delete production symlink"
 		fi
 		
 		# create new symlink
-		if ! ln -s $src $dst; then
-				echo "***** Unable to create production symlink" >&2
-				delete_lock_file_and_exit 5
-		fi
+		ln -s $src $dst || die "Unable to create production symlink"
 }
 
 symlink_post_hook(){
@@ -140,82 +160,87 @@ symlink_post_hook(){
 
 do_symlink(){
 		stage="symlink"
-		echo "***** Stage: ${stage}"
-		if ! symlink_pre_hook; then
-				echo "symlin_pre_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 5
-		fi
-		symlink_update $EXPORT_TARGET/$releasename $SYMLINK_PATH
-		if ! symlink_post_hook; then
-				echo "symlin_post_hook returned non-zero status, aborting"
-				delete_lock_file_and_exit 5
-		fi
-		echo "***** Stage: ${stage} ok"
+		phase_start
+		symlink_pre_hook || die "symlin_pre_hook returned non-zero status, aborting"
+		symlink_update $EXPORT_TARGET/$releasename $SYMLINK_PATH || die
+		symlink_post_hook || die "symlin_post_hook returned non-zero status, aborting"
+		phase_end
+}
+
+# Delete old releases
+do_prune(){
+		stage="prune"
+		phase_start "Checking for old releases to remove"
+		for dir in ${prune[@]}; do
+				rm -rf $dir || die
+		done
+		phase_end
 }
 
 ############## END FUNCTIONS ####################3
 
 if [ $# -gt 1 ]; then
-	echo "Bad usage"
-	exit 48
+		echo "Bad usage"
+		exit 48
 fi
 
 if [ $# -eq 1 ]; then
-	SETTINGS_FILE=$1
+		SETTINGS_FILE=$1
 else
-	DIR=`dirname "$0"`
-	SETTINGS_FILE=$DIR/deploy_settings.sh
+		DIR=`dirname "$0"`
+		SETTINGS_FILE=$DIR/deploy_settings.sh
 fi
 
 if [ ! -e $SETTINGS_FILE ]; then
-	echo "Cannot find settings file. Expected location: $SETTINGS_FILE"
-	exit 47
+		echo "${errstar} Cannot find settings file. Expected location: $SETTINGS_FILE"
+		exit 1
 fi
 
 SETTINGS_FILE_OK=0
 source "$SETTINGS_FILE"
 if [ $SETTINGS_FILE_OK -eq 1 ]; then
-	echo "***** Settings file OK"
+		echo "${infostar} Settings file OK"
 else
-	echo "***** Unable to parse settings file $SETTINGS_FILE" >&2
-	exit 123
+		echo "${errstar}Unable to parse settings file $SETTINGS_FILE" >&2
+		exit 1
 fi
 
 
 # Create release name
 releasename="${releaseprefix:-release}-`date +%Y%m%d%H%M%S`"
+DST="${EXPORT_TARGET}/${releasename}"
 
 # Check that release path exists
 if [ ! -e $EXPORT_TARGET ]; then
-	echo "***** Export target $EXPORT_TARGET does not seem to exist. Aborting."
-	exit 46
+		echo "${errstar} Export target $EXPORT_TARGET does not seem to exist. Aborting."
+		exit 1
 fi
 
 # Check vcs that we support selected vcs:
 case $VCS in
-svn)
-	echo "***** Using SVN"
-	;;
-git)
-	echo "***** Using GIT"
-	;;
-*)
-	echo "***** Unknown vcs specifed: $VCS. Aborting."
-	exit 49
+		svn)
+				echo "${infostar} Using SVN"
+				;;
+		git)
+				echo "${infostar} Using GIT"
+				;;
+		*)
+				echo "${errstar} Unknown vcs specifed: $VCS. Aborting."
+				exit 1
 esac
 
 # abort if the working copy doesn't exist
 if [ ! -e ${RELEASE_WC} ]; then
-		echo "$0: Working copy '${RELEASE_WC}\` does not exist" > /dev/stderr
+		echo "${errstar} Working copy '${RELEASE_WC}\` does not exist"
 		exit 1
 fi
 
 # abort if SYMLINK_PATH exists but is not a symlink
 if [[ ( -e ${SYMLINK_PATH} ) && (! -L ${SYMLINK_PATH}) ]]; then
-		echo "$0: SYMLINK_PATH already exists but it not a symlink. Check configuration and/or remove ${SYMLINK_PATH} manually."
+		echo "${errstar} SYMLINK_PATH already exists but it not a symlink. Check configuration and/or remove ${SYMLINK_PATH} manually."
 		exit 1
 elif [[ (-L ${SYMLINK_PATH}) && (! -e ${SYMLINK_PATH}) ]]; then
-		echo "$0: Warning: ${SYMLINK_PATH} is a dangling symlink."
+		echo "${errstar} Warning: ${SYMLINK_PATH} is a dangling symlink."
 fi
 
 # Create list of old releases to an array called "prune"
@@ -227,67 +252,50 @@ while [[ ${#releases[*]} -gt ${RELEASE_COUNT:-8} ]]; do
 		releases=(${releases[@]:1})
 done
 
+# Check for lock file
+if [ -e $LOCK_FILE ]; then
+    echo "${errstar} Lock file $LOCK_FILE exists, bailing out." >&2
+    exit 1
+fi
+
 # Ask user to confirm
+echo
 echo "*********************************"
 echo "This is what I'll do:"
-echo "touch $LOCK_FILE"
-update_repo_cmd
-export_repo_cmd
-echo "rm $SYMLINK_PATH"
-echo "ln -s $EXPORT_TARGET/$releasename $SYMLINK_PATH"
+echo "  touch $LOCK_FILE"
+echo -n "  "; update_repo_cmd
+echo -n "  "; export_repo_cmd
+echo "  rm $SYMLINK_PATH"
+echo "  ln -s $DST $SYMLINK_PATH"
 for dir in ${prune[@]}; do
-		echo "rm -rf $dir"
+		echo "  rm -rf $dir"
 done
 if [[ ${PRUNE:+1} ]]; then
 		for pattern in "${PRUNE[@]}"; do
-				echo "find \"${EXPORT_TARGET}/$releasename\" -regex \"${pattern}\" -delete"
+				echo "  find \"${DST}\" -regex \"${pattern}\" -delete"
 		done
 fi
-echo "rm $LOCK_FILE"
+echo "  rm $LOCK_FILE"
 echo "*********************************"
+echo
 answer="fail"
-#until (( "$answer" == "yes" )) || (( "$answer" == "no" )); do
 until [ "$answer" == "yes" ]; do
-	echo "Do you wish to continue? Please answer with yes or no."
-	read answer
-	if [ "$answer" == "no" ]; then
-		echo "Aborting."
-		exit 45
-	fi
+		echo -n "${bold}Do you wish to continue?${normal} [${green}yes${normal}/${red}no${normal}] "
+		read answer
+		if [ "$answer" == "no" ]; then
+				echo "Aborting."
+				exit 45
+		fi
 done
-
-# Check for lock file
-if [ -e $LOCK_FILE ]; then
-        echo "***** Lock file $LOCK_FILE exists, bailing out." >&2
-        exit 1
-fi
 
 # Create lock file
-touch $LOCK_FILE
-if [ $? -eq 0 ]; then
-	echo "***** Lock file created"
-else
-	echo "***** Unable to create lock file" >&2
-	exit 2
-fi
+touch $LOCK_FILE || die "Unable to create lock file"
 
 stage=none
-
-# Update checkout
 do_update
-
-# Export
 do_export
-
-# update symlink
 do_symlink
+do_prune
 
-# Delete old releases
-echo "***** Checking for old releases to remove"
-for dir in ${prune[@]}; do
-		rm -rf $dir
-done
-
-# Yay, done!
-echo "***** Done."
-delete_lock_file_and_exit 0
+rm $LOCK_FILE || die
+echo "${infostar} Deploy complete."
